a59857e5b8d7ec54988f3c353e6c97e45cc46795
ssh <kathishivaram@gmail.com>
2019-06-13 18:20:13 +0530

    add gitignore

---
+++ fizzbuzz-testciTests/fizzbuzz_testciTests.swift
@@ -0,0 +1,34 @@
+//
+//  fizzbuzz_testciTests.swift
+//  fizzbuzz-testciTests
+//
+//  Created by apple on 6/13/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import XCTest
+@testable import fizzbuzz_testci
+
+class fizzbuzz_testciTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
